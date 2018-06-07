d1d469ff42d25c4fcd341da7f8b562aa4bbd145e
Alessio Torricelli <Alessio92845@gmail.com>
2018-06-07 17:43:53 +0200

    versione 2.0

--- js/script.js
+++ js/script.js
@@ -1,27 +1,6 @@
-window.onload = function()
-{
-	
-}
-
 function prendiPosizione()
 {
 	var pos = $('#posizione').attr("position");
 
 	$('.testo').attr("value", Math.round(pos.x) + " " + Math.round(pos.y) + " " + Math.round(pos.z));
 }
-/*
-function posizioneCamera()
-{
-	var pos = $('#posizione').attr("position");
-
-	$('.testo').attr('rotation', '0 ' + (Math.PI/2)-(Math.atan(Math.abs(pos.x)/Math.abs(pos.y))) + ' 0');
-}
-*/
-
-function posizioneCamera()
-{/*
-	var pos = $('#posizione').attr("position");
-
-	$('#posizione').attr("position", Math.round(pos.x) + " " + Math.round(pos.y) + " " + Math.round(pos.z));*/
-}
-
